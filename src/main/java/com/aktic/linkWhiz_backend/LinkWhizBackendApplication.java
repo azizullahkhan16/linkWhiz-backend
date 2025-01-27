f9a2caf1a2cb3e23e4619cbe97eabd68358647c0
Azizullah Khan <aziz.bin.aman16@gmail.com>
2025-01-27 16:35:59 +0500

    Project setup

---
+++ src/main/java/com/aktic/linkWhiz_backend/LinkWhizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aktic.linkWhiz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkWhizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkWhizBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aktic/linkWhiz_backend/LinkWhizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aktic.linkWhiz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkWhizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
